c88ff4d9985d2d158a0979c4cf0c971de9da2c08
Ilia Riabko <ilia@Pechka02.>
2025-03-11 22:45:15 +0100

    Add bullet

---
+++ Assets/BulletController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField]
+    public float Speed = 1.0f;
+
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.right * Speed;
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Destroy bullet");
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Debug.Log("Destroy bullet");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/ShootingController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamekit2D;
+
+public class ShootingController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject Bullet;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            float facing = GetComponent<PlayerCharacter>().GetFacing();
+
+            Vector3 bulletPosition = (facing > 0) ? transform.Find("FacingRightBulletSpawnPoint").position : transform.Find("FacingLeftBulletSpawnPoint").position;
+            Quaternion bulletRotation = (facing > 0) ? Quaternion.Euler(0, 0, 0) : Quaternion.Euler(0, 0, 180);
+
+            GameObject bulletInstance = Instantiate(Bullet, bulletPosition, bulletRotation);
+
+        }
+    }
+}
