11988c1cd782880b757f5750472bf378a7bc0bc6
Ilia <ilia_riabko@mail.ru>
2025-03-20 01:37:16 +0100

    Add PowerUp mechanics

---
+++ Assets/MyAssets/PowerUpComponent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class PowerUpComponent : MonoBehaviour
+{
+    public float SizeFactor = 1.0f;
+    public void GetSuperpowers(float duration)
+    {
+        StartCoroutine(ScaleUpAndDown(duration));
+    }
+    private IEnumerator ScaleUpAndDown(float duration)
+    {
+        transform.localScale = SizeFactor * Vector3.one;
+        yield return new WaitForSeconds(duration);
+        transform.localScale = Vector3.one;
+
+    }
+}
---
+++ Assets/MyAssets/PowerUpItemController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PowerUpItemController : MonoBehaviour
+{
+    public UnityEvent itemEffect;
+    public float itemDuration;
+
+    private CapsuleCollider2D trigger;
+    private SpriteRenderer spriteRenderer;
+
+
+    public void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            itemEffect.Invoke();
+            trigger = GetComponent<CapsuleCollider2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            StartCoroutine(SetDisable());
+        }
+    }
+
+    private IEnumerator SetDisable()
+    {
+        trigger.enabled = false;
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.3f);
+        yield return new WaitForSeconds(itemDuration);
+        trigger.enabled = true;
+        StartCoroutine(Flicker());
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1.0f);
+    }
+    private IEnumerator Flicker()
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            spriteRenderer.color = Color.cyan;
+            yield return new WaitForSeconds(0.1f);
+            spriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+}
